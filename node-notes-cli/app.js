78a2ad16b1295742025177a8bac8a910c2ae56dc
Joe King <tiptop1112@gmail.com>
2023-01-25 04:39:54 +0000

    completed add a new note feature

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,11 +1,28 @@
 const myArgs = process.argv.slice(2);
-const json = require('./data.json');
-const parsedNotes = json.notes;
+const json = require('./data.json'); // json is assigned the required data.json file that converts the JSON object to JS object
+const parsedNotes = json.notes; // parsedNotes is assigned the notes javascript object
+let parsedNextId = json.nextId;
 const noteKeys = Object.keys(parsedNotes);
+const fs = require('fs');
+// console.log(`Value of parsedNextId: ${parsedNextId}`);
+// console.log(json);
 
 // reads notes from data.json
 if (myArgs[0] === 'read') {
   noteKeys.forEach(key => {
     console.log(`${key}: ${parsedNotes[key]}`);
   });
+}
+
+// creates new note and appends to data.json
+if (myArgs[0] === 'create') {
+  parsedNotes[parsedNextId] = myArgs[1];
+  parsedNextId++;
+  json.notes = parsedNotes;
+  json.nextId = parsedNextId;
+  const jsonStringify = JSON.stringify(json, null, 2);
+  fs.writeFile('data.json', jsonStringify, err => {
+    if (err) throw err;
+  });
+  // console.log(JSON.stringify(json, null, 2), `parsedNextId increments to: ${parsedNextId}`);
 }
